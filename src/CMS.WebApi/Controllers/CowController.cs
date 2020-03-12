e90795a179a65db2764243de8cc87257e2fd3d43
Kulikowski Konrad <kulikowski.konrad93@gmail.com>
2020-03-12 20:35:31 +0100

    Implement CowController

---
+++ src/CMS.WebApi/Controllers/CowController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.Domain.Models;
+using CMS.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CowController : ControllerBase
+    {
+        private readonly CMSDbContext _context;
+
+        public CowController(CMSDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult New(NewCowCommand command)
+        {
+            try
+            {
+                var cow = new Cow(command.earningNumber, command.dateOfBirth);
+                _context.Add(cow);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Kill(KillCowCommand command)
+        {
+            try
+            {
+                var deathCow = _context.Cows.Where(x => x.Id == command.Id).SingleOrDefault();
+                deathCow.Kill(command.DateOfDeath);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Sell(SellCowCommand command)
+        {
+            try
+            {
+                var cowToSell = _context.Cows.Where(x => x.Id == command.Id).SingleOrDefault();
+                cowToSell.Sell(command.Price, command.Weight, command.DateOfSold);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Buy(BuyCowCommand command)
+        {
+            try
+            {
+                var cow = Cow.FromBought(command.Price, command.Weight, command.DateOfBirth, command.EarningNumber);
+                _context.Add(cow);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+
+    public class NewCowCommand
+    {
+        public string earningNumber { get; set; }
+        public DateTime dateOfBirth { get; set; }
+    }
+
+    public class KillCowCommand
+    {
+        public long Id { get; set; }
+        public DateTime DateOfDeath { get; set; }
+    }
+
+    public class SellCowCommand
+    {
+        public long Id { get; set; }
+        public double Price { get; set; }
+        public double Weight { get; set; }
+        public DateTime DateOfSold { get; set; }
+    }
+
+    public class BuyCowCommand
+    {
+        public double Price { get; set; }
+        public double Weight { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EarningNumber { get; set; }
+    }
+}
--- src/CMS.WebApi/Startup.cs
+++ src/CMS.WebApi/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMS.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace CMS.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<CMSDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CawshedManagementDbConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
