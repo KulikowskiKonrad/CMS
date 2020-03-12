65b5a90304bdadc4777663fdd83c910ad3c09b84
Kulikowski Konrad <kulikowski.konrad93@gmail.com>
2020-03-12 20:35:20 +0100

    Add DbContext and migration

---
+++ src/CMS.Infrastructure/CMSDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CMS.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS.Infrastructure
+{
+    public class CMSDbContext : DbContext
+    {
+        public DbSet<Cow> Cows { get; set; }
+
+        public CMSDbContext(Microsoft.EntityFrameworkCore.DbContextOptions options) : base(options) { }
+    }
+}
---
+++ src/CMS.Infrastructure/Migrations/20200311211016_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CMS.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cows",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EarningNumber = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<short>(nullable: false),
+                    Weight = table.Column<double>(nullable: true),
+                    SoldPrice = table.Column<double>(nullable: true),
+                    BoughtPrice = table.Column<double>(nullable: true),
+                    DateOfSold = table.Column<DateTime>(nullable: true),
+                    DateOfDeath = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cows", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cows");
+        }
+    }
+}
