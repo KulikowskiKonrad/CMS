fbda685d5f638e4c36c7199be87fb1282e41232c
Kulikowski Konrad <kulikowski.konrad93@gmail.com>
2020-03-16 22:41:31 +0100

    test

--- src/CMS.Infrastructure/TypesConfiguration/InseminationEntityConfiguration.cs
+++ src/CMS.Infrastructure/TypesConfiguration/InseminationEntityConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using CMS.Domain.Models.CowAggregate;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CMS.Infrastructure.TypesConfiguration
 {
