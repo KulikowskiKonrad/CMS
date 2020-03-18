5f2074a27a7ad1067f63bd416c51ec5eb5c07ba0
Kulikowski Konrad <kulikowski.konrad93@gmail.com>
2020-03-18 19:37:39 +0100

    Get ready to CI/DI

---
+++ src/CMS.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index() => Ok("Hello World!");
+    }
+}
